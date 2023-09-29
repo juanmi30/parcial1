f4a15c36e1ac91b71e39c4e6e7a099ce6aaa1cdd
Juan Miguel Manrique <j.manriquec@uniandes.edu.co>
2023-09-29 09:30:01 -0500

    Final

--- src/consola/Aplicacion.java
+++ src/consola/Aplicacion.java
@@ -1,8 +1,43 @@
 package consola;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import procesamiento.Tablero;
+
 public class Aplicacion {
+
+	public static String input(String mensaje) {
+		try {
+			System.out.println(mensaje + ": ");
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			return reader.readLine();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
 	public static void main(String[] args) {
+
+		System.out.println("Simulador tablero de ajedrez");
+
+		String jugar = input("Presione 1 para jugar");
 		
+		if (Integer.parseInt(jugar) == 1) {
+			System.out.println("Escoger pieza para jugar:");
+			System.out.println("1. Peon\n2. Rey\n3. Torre");
+			String piezaSeleccion = input("seleccione");
+			if (Integer.parseInt(piezaSeleccion) == 1) {
+				Tablero tablero = new Tablero(8, 8 , "peon");
+			} else if (Integer.parseInt(piezaSeleccion)== 2) {
+				Tablero tablero = new Tablero(8, 8 , "rey");
+			} else if (Integer.parseInt(piezaSeleccion)==3) {
+				Tablero tablero = new Tablero(8, 8 , "torre");
+			}
+		}
 
 	}
 
--- src/procesamiento/Tablero.java
+++ src/procesamiento/Tablero.java
@@ -1,6 +1,47 @@
 package procesamiento;
 
+import java.util.ArrayList;
+
+import modelo.Ficha;
+import modelo.Peon;
+
 public class Tablero {
 	private int nFilas;
 	private int nColumnas;
+	private String fichaEnJuego;
+	
+	public Tablero(int nFilas, int nColumnas, String string) {
+		this.nFilas = nFilas;
+		this.nColumnas = nColumnas;
+		this.fichaEnJuego = string;
+	}
+
+	public int getnFilas() {
+		return nFilas;
+	}
+
+	public void setnFilas(int nFilas) {
+		this.nFilas = nFilas;
+	}
+
+	public int getnColumnas() {
+		return nColumnas;
+	}
+
+	public void setnColumnas(int nColumnas) {
+		this.nColumnas = nColumnas;
+	}
+	
+	public void iniciarJuego(String ficha) {
+		boolean jugar = true;
+		if (ficha == "peon") {
+			Peon peon = new Peon("peon", 0, 0, true);
+		while (jugar) {
+			peon.moverAdelante();
+		}
+		
+		}
+
+	}
+	
 }
